620e8b9267f19713793159aaeac7ddd664b18099
Marius Vaitkus <Marius.Vaitkus@4finance.com>
2015-06-05 15:42:14 +0300

    Setup doesn't overwrite existing config

--- setup_pgw.sh
+++ setup_pgw.sh
@@ -4,7 +4,7 @@ sudo apt-get install python3-pip
 sudo pip3 install pexpect argcomplete
 mkdir -p ~/bin
 ln -s `pwd`/pgw ~/bin/pgw
-cp .pgw_config ~/
+cp -n .pgw_config ~/
 
 echo -e '\n# Registers autocompleter for pgw' >> ~/.bashrc
 echo 'eval "$(register-python-argcomplete pgw)"' >> ~/.bashrc
